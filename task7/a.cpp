27e8113b1a8d4451c0d887fec20baf2e7c6e42bd
Yakutov Dmitry <miti_95@mail.ru>
2017-04-01 00:43:03 +0200

    task7

--- task7/a.cpp
+++ task7/a.cpp
@@ -8,5 +8,7 @@ int main()
     lazy_string a(std::string("abacaba"));
     lazy_string b = a.substr(1);
     lazy_string c = b.substr(1);
+    lazy_string::reference x = c[3];
+    x = 'X';
     cout << a << ' ' << b << ' ' << c << '\n';
 }
